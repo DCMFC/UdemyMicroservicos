1a95bcd310f7307fc973e9f394627f46b5322bb2
dcmfc <dayanecristine.caldeira@gmail.com>
2019-03-04 20:31:51 -0300

    continuacao curso microserviços

--- currency-conversion-service/src/main/java/com/dcmfc/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/dcmfc/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.dcmfc.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.dcmfc.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
