5542ad28429aa081b57328c93cb898c1d1aedd9d
dcmfc <dayanecristine.caldeira@gmail.com>
2019-05-07 20:24:32 -0300

    update

---
+++ spring-boot-in-10-steps/src/main/java/com/dcmfc/springboot/springbootin10steps/BooksController.java
@@ -0,0 +1,18 @@
+package com.dcmfc.springboot.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks()
+	{
+		return Arrays.asList(new Book(1L, "Livro 1", "Autor 1"));
+	}
+
+}
